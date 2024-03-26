f4abe62b6c1abdb675c708967d18b6b02de47f9c
Ezequiel Oggioni <oggioni@providencia.com.ar>
2024-03-25 21:44:49 -0300

    otra vez

--- src/app/sumadora.ts
+++ src/app/sumadora.ts
@@ -1,16 +1,16 @@
 export class Sumadora {
-    public numero1: number;
+  public numero1: number;
   public numero2: number;
   public resultado: number;
   public operacion: number = 0;
 
-    constructor() {
-        this.numero1 = 0;
-        this.numero2 = 0;
-        this.resultado = 0;
-      }
-      
- public  calcular() {
+  constructor() {
+    this.numero1 = 0;
+    this.numero2 = 0;
+    this.resultado = 0;
+  }
+
+  public calcular() {
     this.operacion = parseInt(this.operacion.toString());
     switch (this.operacion) {
       case 1:
@@ -28,7 +28,21 @@ export class Sumadora {
       default:
         this.resultado = 0;
     }
+    this.numero1 =this.resultado;
+    this.numero2 = 0;
   }
 
-    
+  public concatenar(valor: number) {
+    this.numero2 = this.numero2 * 10 + parseInt(valor.toString());
+  }
+  public limpiar() {
+    this.numero2 = 0;
+  }
+
+  public realizarOperacion(operacion: number) {
+    this.operacion = parseInt(operacion.toString());
+    this.numero1 = this.numero2;
+    this.limpiar();
+  }
+
 }
